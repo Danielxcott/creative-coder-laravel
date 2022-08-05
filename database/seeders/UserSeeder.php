2fd47657359ac90d09ed017112eb175cdc8ba4a4
Thein Zan <95298352+Danielxcott@users.noreply.github.com>
2022-08-06 01:39:04 +0630

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,7 +21,7 @@ class UserSeeder extends Seeder
             'username'=> "lucien",
             'email' => 'zan@gmail.com',
             'is_admin' => '1',
-            'password' => Hash::make('littlezan'),
+            'password' => Hash::make('12345678'),
         ]);
     }
 }
